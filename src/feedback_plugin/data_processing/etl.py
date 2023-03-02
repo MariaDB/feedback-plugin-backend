c133b4817836aaba05e949d0446c5d1b0ea235cb
Vicențiu Ciorbaru <cvicentiu@gmail.com>
2023-03-02 15:58:21 +0200

    Cleanup data processing for pyflakes

--- src/feedback_plugin/data_processing/etl.py
+++ src/feedback_plugin/data_processing/etl.py
@@ -1,232 +1,200 @@
-import csv
-import re
 from collections import defaultdict
-from datetime import datetime, timezone
+from datetime import datetime
 from io import StringIO
+import csv
+
+from django.db.models import Q
 
 from feedback_plugin.models import (ComputedServerFact, ComputedUploadFact,
                                     Data, RawData, Server, Upload)
-from django.db.models import Q
+from .extractors import DataExtractor
+
 
 def process_raw_data():
-  for raw_upload in RawData.objects.order_by('upload_time').iterator():
+    for raw_upload in RawData.objects.order_by('upload_time').iterator():
+        country_code = raw_upload.country
+        raw_upload_time = raw_upload.upload_time
+        raw_data = StringIO(raw_upload.data.decode('utf-8')
+                            .replace('\x00', ''))
 
-    country_code = raw_upload.country
-    raw_upload_time = raw_upload.upload_time
-    raw_data = StringIO(raw_upload.data.decode('utf-8').replace('\x00', ''))
+        reader = csv.reader(raw_data, delimiter='\t')
 
-    reader = csv.reader(raw_data, delimiter='\t')
+        values = []
+        data = {}
+        for row in reader:
+            # We only expect KV pairs.
+            if len(row) != 2:
+                continue
 
-    values = []
-    data = {}
-    for row in reader:
-      # We only expect KV pairs.
-      if len(row) != 2:
-        continue
+            data[row[0]] = row[1]
+            values.append(Data(key=row[0], value=row[1]))
 
-      data[row[0]] = row[1]
-      values.append(Data(key=row[0], value=row[1]))
+        if ('FEEDBACK_SERVER_UID' not in data
+                or len(data['FEEDBACK_SERVER_UID']) == 0):
+            continue
 
-    if ('FEEDBACK_SERVER_UID' not in data
-        or len(data['FEEDBACK_SERVER_UID']) == 0):
-      continue
+        if ('FEEDBACK_USER_INFO' in data
+                and data['FEEDBACK_USER_INFO'] == 'mysql-test'):
+            continue
 
-    if ('FEEDBACK_USER_INFO' in data
-        and data['FEEDBACK_USER_INFO'] == 'mysql-test'):
-      continue
+        uid = data['FEEDBACK_SERVER_UID']
+        try:
+            srv_uid_fact = ComputedServerFact.objects.get(key='uid', value=uid)
+            server = Server.objects.get(id=srv_uid_fact.server.id)
+        except ComputedServerFact.DoesNotExist:
+            server = Server()
+            srv_uid_fact = ComputedServerFact(key='uid',
+                                              value=uid,
+                                              server=server)
+            server.save()
+            srv_uid_fact.save()
 
-    uid = data['FEEDBACK_SERVER_UID']
-    try:
-      srv_uid_fact = ComputedServerFact.objects.get(key='uid', value=uid)
-      server = Server.objects.get(id=srv_uid_fact.server.id)
-    except ComputedServerFact.DoesNotExist:
-      server = Server()
-      srv_uid_fact = ComputedServerFact(key='uid',
-                                        value=uid,
-                                        server=server)
-      server.save()
-      srv_uid_fact.save()
+        except Server.DoesNotExist:
+            # We have a fact, but not a server attached to it. This should
+            # never happen.
+            assert False
 
-    except Server.DoesNotExist:
-      # We have a fact, but not a server attached to it. This should never
-      # happen.
-      assert(0)
+        srv_facts = []
 
-    srv_facts = []
-    def get_fact_by_key(key, server, value):
-      try:
-        srv_fact = ComputedServerFact.objects.get(key=key,
-                                                  server__id=server.id)
-        srv_fact.value = value
-        return (False, srv_fact)
-      except ComputedServerFact.DoesNotExist:
-        return (True, ComputedServerFact(key=key, value=value, server=server))
+        def get_fact_by_key(key, server, value):
+            try:
+                srv_fact = ComputedServerFact.objects.get(key=key,
+                                                          server__id=server.id)
+                srv_fact.value = value
+                return (False, srv_fact)
+            except ComputedServerFact.DoesNotExist:
+                return (True, ComputedServerFact(key=key,
+                                                 value=value,
+                                                 server=server))
 
-    srv_facts.append(get_fact_by_key('country_code', server, country_code))
-    srv_facts.append(get_fact_by_key('last_seen', server, raw_upload_time))
-    srv_facts.append(get_fact_by_key('first_seen', server, raw_upload_time))
-    # If first_seen already existed, we don't override it.
-    if (srv_facts[-1][0] == False):
-      srv_facts.pop()
+        srv_facts.append(get_fact_by_key('country_code', server, country_code))
+        srv_facts.append(get_fact_by_key('last_seen', server, raw_upload_time))
+        srv_facts.append(get_fact_by_key('first_seen', server, raw_upload_time))
+        # If first_seen already existed, we don't override it.
+        if (srv_facts[-1][0] is False):
+            srv_facts.pop()
 
-    srv_facts_create = map(lambda x: x[1], filter(lambda x: x[0], srv_facts))
-    srv_facts_update = map(lambda x: x[1], filter(lambda x: not x[0], srv_facts))
+        srv_facts_create = map(lambda x: x[1],
+                               filter(lambda x: x[0], srv_facts))
+        srv_facts_update = map(lambda x: x[1],
+                               filter(lambda x: not x[0], srv_facts))
 
-    # Create Server computed facts for this RawData.
-    ComputedServerFact.objects.bulk_create(srv_facts_create,
-                                           batch_size=1000)
-    ComputedServerFact.objects.bulk_update(srv_facts_update, ['value'],
-                                           batch_size=1000)
+        # Create Server computed facts for this RawData.
+        ComputedServerFact.objects.bulk_create(srv_facts_create,
+                                               batch_size=1000)
+        ComputedServerFact.objects.bulk_update(srv_facts_update, ['value'],
+                                               batch_size=1000)
 
-    # Create Upload and all Data entries for this RawData.
-    upload = Upload(upload_time=raw_upload_time, server=server)
+        # Create Upload and all Data entries for this RawData.
+        upload = Upload(upload_time=raw_upload_time, server=server)
 
-    for i in range(len(values)):
-      values[i].upload = upload
-    upload.save()
-    Data.objects.bulk_create(values)
+        for i in range(len(values)):
+            values[i].upload = upload
+        upload.save()
+        Data.objects.bulk_create(values)
 
-  RawData.objects.all().delete()
-
-
-def compute_upload_facts(start_date, end_date):
-
-  data_to_process = Data.objects.filter(upload__upload_time__gte=start_date,
-                      upload__upload_time__lte=end_date,
-                      key='VERSION')
-
-  pattern = re.compile('(?P<major>\d+).(?P<minor>\d+).(?P<point>\d+)')
-  version_facts = []
-  version_dict = {}
-
-  for data in data_to_process:
-    matches = pattern.match(data.value)
-    if (data.upload_id not in version_dict):
-      version_dict[data.upload_id] = {}
-
-    version_dict[data.upload_id]['major'] = ComputedUploadFact(
-                         key='version_major',
-                         value=matches.group('major'),
-                         upload=data.upload)
-    version_dict[data.upload_id]['minor'] = ComputedUploadFact(
-                         key='version_minor',
-                         value=matches.group('minor'),
-                         upload=data.upload)
-    version_dict[data.upload_id]['point'] = ComputedUploadFact(
-                         key='version_point',
-                         value=matches.group('point'),
-                         upload=data.upload)
-
-  for upload_id, version in version_dict.items():
-    version_facts.append(version['major'])
-    version_facts.append(version['minor'])
-    version_facts.append(version['point'])
-
-  ComputedUploadFact.objects.bulk_create(version_facts, batch_size=1000)
+    RawData.objects.all().delete()
 
 
 def get_upload_data_for_data_extractors(start_date, end_date,
                                         data_extractors):
-  keys = set()
-  for extractor in data_extractors:
-    keys |= extractor.get_required_keys()
-  key_filter = Q()
-  for key in keys:
-    key_filter |= Q(key__iexact=key)
+    keys = set()
+    for extractor in data_extractors:
+        keys |= extractor.get_required_keys()
+    key_filter = Q()
+    for key in keys:
+        key_filter |= Q(key__iexact=key)
 
-  # Extract server data about Architecture and OS
-  data_to_process = (Data.objects
-      .filter(Q(upload__upload_time__gte=start_date) &
-              Q(upload__upload_time__lte=end_date) &
-              key_filter)
-      .select_related('upload__server')
-  )
+    # Extract server data about Architecture and OS
+    data_to_process = Data.objects.filter(
+        Q(upload__upload_time__gte=start_date) &
+        Q(upload__upload_time__lte=end_date) &
+        key_filter
+    ).select_related('upload__server')
 
-  servers = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
-  for data in data_to_process:
-    # In case there are multiple entries for the same server, use the
-    # latest one only
-    server_id = data.upload.server.id
-    upload_id = data.upload.id
-    # Appending to a list allows for multiple values for the same key.
-    servers[server_id][upload_id][data.key.lower()].append(data.value)
+    servers = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    for data in data_to_process:
+        # In case there are multiple entries for the same server, use the
+        # latest one only
+        server_id = data.upload.server.id
+        upload_id = data.upload.id
+        # Appending to a list allows for multiple values for the same key.
+        servers[server_id][upload_id][data.key.lower()].append(data.value)
 
-  return servers
-
+    return servers
 
 
 def extract_server_facts(start_date, end_date, data_extractors):
-  servers = get_upload_data_for_data_extractors(start_date, end_date,
-                                                data_extractors)
-  facts = defaultdict(dict)
-  for extractor in data_extractors:
-    new_facts = extractor.extract_facts(servers)
-    for s_id in new_facts:
-      facts[s_id].update(new_facts[s_id])
+    servers = get_upload_data_for_data_extractors(start_date, end_date,
+                                                  data_extractors)
+    facts = defaultdict(dict)
+    for extractor in data_extractors:
+        new_facts = extractor.extract_facts(servers)
+        for s_id in new_facts:
+            facts[s_id].update(new_facts[s_id])
 
-  # Arrange all facts { 'key' : { server_id : value ... } }
-  facts_by_key = defaultdict(dict)
-  for server_id in facts:
-    for key in facts[server_id]:
-      facts_by_key[key][server_id] = facts[server_id][key]
+    # Arrange all facts { 'key' : { server_id : value ... } }
+    facts_by_key = defaultdict(dict)
+    for server_id in facts:
+        for key in facts[server_id]:
+            facts_by_key[key][server_id] = facts[server_id][key]
+
+    # We insert all computed facts with a bulk-insert per-key.
+    for key in facts_by_key:
+        servers_with_computed_fact = set(facts_by_key[key].keys())
+
+        # To avoid inserting for every individual fact and retrying
+        # with an update if the fact already exists, compute a list of
+        # facts already present and call update for those via bulk_update.
+        #
+        # This query returns only one fact per server because the key is
+        # unique.
+        #
+        # TODO(cvicentiu) This could be optimized by creating a unique key
+        # for each ComputedServerFact (server_id, key) and relying on
+        # the database to "ignore" updates.
+        # This optimization only works for server facts that do not change
+        # over the lifespan of the server.
+        facts_already_in_db = ComputedServerFact.objects.filter(
+            server__id__in=servers_with_computed_fact,
+            key=key)
+
+        facts_in_db_by_s_id = {}
+        for fact in facts_already_in_db:
+            facts_in_db_by_s_id[fact.server.id] = fact
+
+        # In order to not do an insert for every individual fact, compute
+        # a list of facts that need to be updated and a list of facts
+        # that need to be created.
+        facts_create = []
+        facts_update = []
+        for s_id in servers_with_computed_fact:
+            fact_value = facts_by_key[key][s_id]
+
+            if s_id in facts_in_db_by_s_id:
+                facts_in_db_by_s_id[s_id].value = fact_value
+                facts_update.append(facts_in_db_by_s_id[s_id])
+            else:
+                computed_fact = ComputedServerFact(key=key, value=fact_value,
+                                                   server_id=s_id)
+                facts_create.append(computed_fact)
+
+        ComputedServerFact.objects.bulk_create(facts_create,
+                                               batch_size=1000)
+        ComputedServerFact.objects.bulk_update(facts_update, ['value'],
+                                               batch_size=1000)
 
 
-  # We insert all computed facts with a bulk-insert per-key.
-  for key in facts_by_key:
-    servers_with_computed_fact = set(facts_by_key[key].keys())
-
-    # To avoid inserting for every individual fact and retrying
-    # with an update if the fact already exists, compute a list of
-    # facts already present and call update for those via bulk_update.
-    #
-    # This query returns only one fact per server because the key is
-    # unique.
-    #
-    # TODO(cvicentiu) This could be optimized by creating a unique key
-    # for each ComputedServerFact (server_id, key) and relying on
-    # the database to "ignore" updates.
-    # This optimization only works for server facts that do not change
-    # over the lifespan of the server.
-    facts_already_in_db = (ComputedServerFact.objects
-        .filter(server__id__in=servers_with_computed_fact,
-                key=key))
-
-    facts_in_db_by_s_id = {}
-    for fact in facts_already_in_db:
-      facts_in_db_by_s_id[fact.server.id] = fact
-
-    # In order to not do an insert for every individual fact, compute
-    # a list of facts that need to be updated and a list of facts
-    # that need to be created.
-    facts_create = []
-    facts_update = []
-    for s_id in servers_with_computed_fact:
-      fact_value = facts_by_key[key][s_id]
-
-      if s_id in facts_in_db_by_s_id:
-        facts_in_db_by_s_id[s_id].value = fact_value
-        facts_update.append(facts_in_db_by_s_id[s_id])
-      else:
-        computed_fact = ComputedServerFact(key=key, value=fact_value,
-                                           server_id=s_id)
-        facts_create.append(computed_fact)
-
-    ComputedServerFact.objects.bulk_create(facts_create,
-                                           batch_size=1000)
-    ComputedServerFact.objects.bulk_update(facts_update, ['value'],
-                                           batch_size=1000)
-
-
-def check_if_upload_fact_exists(key : str, upload_id : int):
+def check_if_upload_fact_exists(key: str, upload_id: int):
     try:
         return ComputedUploadFact.objects.get(key=key, upload_id=upload_id)
     except ComputedUploadFact.DoesNotExist:
         return None
 
 
-
-
-def extract_upload_facts(start_date, end_date, data_extractors):
+def extract_upload_facts(start_date: datetime,
+                         end_date: datetime,
+                         data_extractors: DataExtractor):
     servers = get_upload_data_for_data_extractors(start_date, end_date,
                                                   data_extractors)
 
@@ -243,9 +211,9 @@ def extract_upload_facts(start_date, end_date, data_extractors):
             for key in facts[s_id][upload_id]:
                 fact_value = facts[s_id][upload_id][key]
 
-                #TODO(cvicentiu) This is a rather slow check, it does one database
-                # lookup per upload_id. This should be optimized for faster
-                # processing.
+                # TODO(cvicentiu) This is a rather slow check, it does one
+                # database lookup per upload_id. This should be optimized for
+                # faster processing.
                 up_fact = check_if_upload_fact_exists(key, upload_id)
                 if up_fact is None:
                     up_fact = ComputedUploadFact(key=key, value=fact_value,
--- src/feedback_plugin/data_processing/extractors.py
+++ src/feedback_plugin/data_processing/extractors.py
@@ -5,6 +5,7 @@ import inspect
 import re
 import sys
 
+
 class DataExtractor(ABC):
     @abstractmethod
     def get_required_keys(self):
@@ -18,132 +19,136 @@ class DataExtractor(ABC):
 class UploadFactExtractor(DataExtractor):
     pass
 
+
 class ServerFactExtractor(DataExtractor):
     pass
 
+
 class ArchitectureExtractor(ServerFactExtractor):
+    def get_required_keys(self):
+        return {'uname_machine', 'uname_sysname', 'uname_version',
+                'uname_distribution', 'uname_release'}
 
-  def get_required_keys(self):
-    return {'uname_machine', 'uname_sysname', 'uname_version',
-            'uname_distribution', 'uname_release'}
+    @staticmethod
+    def extract_distribution(upload):
+        if 'uname_distribution' not in upload:
+            return None
 
-  @staticmethod
-  def extract_distribution(upload):
-    if 'uname_distribution' not in upload:
-      return None
+        distro_string = upload['uname_distribution'][0].lower()
+        distro_keywords = [
+            (['archlinux'], 'ArchLinux'),
+            (['centos', 'rhel'], 'CentOS'),
+            (['fedora'], 'Fedora'),
+            (['gentoo'], 'Gentoo'),
+            (['mint'], 'Linux Mint'),
+            (['redhat', 'rhel'], 'Red Hat Enterprise Linux'),
+            (['ubuntu'], 'Ubuntu'),
+            # TODO: fill in more names that need to be cleaned up.
+        ]
 
-    distro_string = upload['uname_distribution'][0].lower()
-    distro_keywords = [
-        (['archlinux'], 'ArchLinux'),
-        (['centos', 'rhel'], 'CentOS'),
-        (['fedora'], 'Fedora'),
-        (['gentoo'], 'Gentoo'),
-        (['mint'], 'Linux Mint'),
-        (['redhat', 'rhel'], 'Red Hat Enterprise Linux'),
-        (['ubuntu'], 'Ubuntu'),
-        # TODO: fill in more names that need to be cleaned up.
-    ]
+        for (patterns, pretty_form) in distro_keywords:
+            for pattern in patterns:
+                if pattern in distro_string:
+                    return pretty_form
+        return distro_string
 
-    for (patterns, pretty_form) in distro_keywords:
-      for pattern in patterns:
-        if pattern in distro_string:
-          return pretty_form
-    return distro_string
+    @staticmethod
+    def extract_machine_architecture(upload):
+        if 'uname_machine' not in upload:
+            return None
 
-  @staticmethod
-  def extract_machine_architecture(upload):
-    if 'uname_machine' not in upload:
-      return None
+        machine = upload['uname_machine'][0].lower()
+        if re.match('^(x(86_)?64)|(amd64)$', machine):
+            machine_architecture = 'x86_64'
+        elif re.match('^[ix][3-6]*86$', machine):
+            # This check must happen after x86_64
+            machine_architecture = 'x86'
+        elif re.match('^armv[5-7]', machine):
+            machine_architecture = 'ARM 32Bit'
+        elif re.match('^aarch64$', machine):
+            machine_architecture = 'ARM 64Bit'
+        elif re.match('^hp_', machine):
+            machine_architecture = 'HP Itanium'
+        elif re.match('^alpha', machine):
+            machine_architecture = 'Alpha'
+        elif re.match('^mips$', machine):
+            machine_architecture = 'MIPS'
+        else:
+            machine_architecture = machine
 
-    machine = upload['uname_machine'][0].lower()
-    if re.match('^(x(86_)?64)|(amd64)$', machine):
-      machine_architecture = 'x86_64'
-    elif re.match('^[ix][3-6]*86$', machine): # This check must happen after x86_64
-      machine_architecture = 'x86'
-    elif re.match('^armv[5-7]', machine):
-      machine_architecture = 'ARM 32Bit'
-    elif re.match('^aarch64$', machine):
-      machine_architecture = 'ARM 64Bit'
-    elif re.match('^hp_', machine):
-      machine_architecture = 'HP Itanium'
-    elif re.match('^alpha', machine):
-      machine_architecture = 'Alpha'
-    elif re.match('^mips$', machine):
-      machine_architecture = 'MIPS'
-    else:
-      machine_architecture = machine
+        return machine_architecture
 
-    return machine_architecture
+    @staticmethod
+    def extract_operating_system(upload):
+        if 'uname_sysname' not in upload:
+            return None
 
-  @staticmethod
-  def extract_operating_system(upload):
-    if 'uname_sysname' not in upload:
-      return None
+        sysname = upload['uname_sysname'][0].lower()
+        if 'linux' in sysname:
+            operating_system = 'Linux'
+        elif 'windows' in sysname:
+            operating_system = 'Windows'
+        elif 'freebsd' in sysname:
+            operating_system = 'FreeBSD'
+        elif 'darwin' in sysname:
+            operating_system = 'OSX'
+        else:
+            operating_system = 'unknown'
 
-    sysname = upload['uname_sysname'][0].lower()
-    if 'linux' in sysname:
-      operating_system = 'Linux'
-    elif 'windows' in sysname:
-      operating_system = 'Windows'
-    elif 'freebsd' in sysname:
-      operating_system = 'FreeBSD'
-    elif 'darwin' in sysname:
-      operating_system = 'OSX'
-    else:
-      operating_system = 'unknown'
+        return operating_system
 
-    return operating_system
+    @staticmethod
+    def extract_os_version(upload):
+        if 'uname_version' not in upload:
+            return None
 
-  def extract_os_version(upload):
-    if 'uname_version' not in upload:
-      return None
+        version_string = upload['uname_version'][0].lower()
 
-    version_string = upload['uname_version'][0].lower()
-    #TODO(cvicentiu): Hack to clean up version strings. This will need
-    # to be changed to cover and extract a wide range of data points.
-    if 'smp' in version_string:
-      version_string = 'unknown'
-      try:
-        version_string = 'unknown'
-        distribution_string = ''
-        if 'uname_distribution' in upload:
-          distribution_string = upload['uname_distribution'][0].lower()
+        # TODO(cvicentiu): Hack to clean up version strings. This will need
+        # to be changed to cover and extract a wide range of data points.
+        if 'smp' in version_string:
+            version_string = 'unknown'
+            try:
+                version_string = 'unknown'
+                distro_string = ''
+                if 'uname_distribution' in upload:
+                    distro_string = upload['uname_distribution'][0].lower()
 
-        # Crude expression for CentOS 8
-        if 'linux release' in distribution_string:
-          first_digit = re.search('[0-9]+', distribution_string)
-          if first_digit is not None:
-            version_string = distribution_string[first_digit.start():]
+                # Crude expression for CentOS 8
+                if 'linux release' in distro_string:
+                    first_digit = re.search('[0-9]+', distro_string)
+                    if first_digit is not None:
+                        version_string = distro_string[first_digit.start():]
 
-      except KeyError:
-        pass
+            except KeyError:
+                pass
 
-    return version_string
+        return version_string
 
-  def extract_facts(self, servers):
-    result = {}
+    def extract_facts(self, servers):
+        result = {}
 
-    for server_id, server_uploads in servers.items():
-      facts = {}
-      for upload_id, upload in server_uploads.items():
-        fact = ArchitectureExtractor.extract_operating_system(upload)
-        if fact is not None:
-          facts['operating_system'] = fact
+        for server_id, server_uploads in servers.items():
+            facts = {}
+            for upload_id, upload in server_uploads.items():
+                fact = ArchitectureExtractor.extract_operating_system(upload)
+            if fact is not None:
+                facts['operating_system'] = fact
 
-        fact = ArchitectureExtractor.extract_machine_architecture(upload)
-        if fact is not None:
-          facts['hardware_architecture'] = fact
+            fact = ArchitectureExtractor.extract_machine_architecture(upload)
+            if fact is not None:
+                facts['hardware_architecture'] = fact
 
-        fact = ArchitectureExtractor.extract_distribution(upload)
-        if fact is not None:
-          facts['distribution'] = fact
+            fact = ArchitectureExtractor.extract_distribution(upload)
+            if fact is not None:
+                facts['distribution'] = fact
 
-        fact = ArchitectureExtractor.extract_os_version(upload)
-        if fact is not None:
-          facts['operating_system_version'] = fact
+            fact = ArchitectureExtractor.extract_os_version(upload)
+            if fact is not None:
+                facts['operating_system_version'] = fact
 
-      result[server_id] = facts
-    return result
+            result[server_id] = facts
+        return result
 
 
 class ServerVersionExtractor(UploadFactExtractor):
@@ -151,20 +156,19 @@ class ServerVersionExtractor(UploadFactExtractor):
     def extract_server_version(upload):
         if 'version' not in upload or len(upload['version']) == 0:
             return None
-        pattern = re.compile('(?P<major>\d+).(?P<minor>\d+).(?P<point>\d+)')
+        pattern = re.compile('(?P<major>\\d+).(?P<minor>\\d+).(?P<point>\\d+)')
 
         # We always take the last entry from a CSV if there happen to be
         # duplicates.
         matches = pattern.match(upload['version'][-1])
 
         result = {
-            'server_version_major' : matches.group('major'),
-            'server_version_minor' : matches.group('minor'),
-            'server_version_point' : matches.group('point'),
+            'server_version_major': matches.group('major'),
+            'server_version_minor': matches.group('minor'),
+            'server_version_point': matches.group('point'),
         }
 
-        #TODO(cvicentiu) handle invalid version
-
+        # TODO(cvicentiu) handle invalid version
         return result
 
     def get_required_keys(self):
@@ -185,8 +189,6 @@ class ServerVersionExtractor(UploadFactExtractor):
 
 class AllFactExtractor(DataExtractor):
     def __init__(self, class_type: type):
-        current_module = sys.modules[__name__]
-
         def class_filter(member):
             if (inspect.isclass(member)
                     and issubclass(member, class_type)
@@ -214,9 +216,11 @@ class AllFactExtractor(DataExtractor):
             result |= extractor.get_required_keys()
         return result
 
+
 class AllUploadFactExtractor(AllFactExtractor):
     def __init__(self):
         super().__init__(UploadFactExtractor)
+
 
 class AllServerFactExtractor(AllFactExtractor):
     def __init__(self):
