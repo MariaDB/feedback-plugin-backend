da58ea1f84d9602fdbdd6161ba8853ccf4631367
Vicențiu Ciorbaru <cvicentiu@gmail.com>
2023-03-15 12:14:00 +0200

    Extract_upload_facts always processes in chunks of 24 hours, even if the interval is greater

--- src/feedback_plugin/management/commands/extract_upload_facts.py
+++ src/feedback_plugin/management/commands/extract_upload_facts.py
@@ -1,9 +1,8 @@
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 
 from django.core.management.base import BaseCommand
 
-from feedback_plugin.data_processing import etl
-from feedback_plugin.data_processing import extractors
+from ...data_processing import etl, extractors
 
 
 class Command(BaseCommand):
@@ -18,5 +17,17 @@ class Command(BaseCommand):
         parser.add_argument('end_time', type=Command.date_with_tz_from_str)
 
     def handle(self, *args, **options):
-        etl.extract_upload_facts(options['start_time'], options['end_time'],
-                                 [extractors.ServerVersionExtractor()])
+        start_time = options['start_time']
+        end_time = options['end_time']
+
+        while start_time + timedelta(seconds=60 * 60 * 24) <= end_time:
+            cur_end = start_time + timedelta(seconds=60 * 60 * 24)
+
+            etl.extract_upload_facts(start_time, cur_end,
+                                     [extractors.AllUploadFactExtractor()])
+            start_time = cur_end
+
+        etl.extract_upload_facts(start_time, end_time,
+                                 [extractors.AllUploadFactExtractor()],
+                                 end_inclusive=True)
+
