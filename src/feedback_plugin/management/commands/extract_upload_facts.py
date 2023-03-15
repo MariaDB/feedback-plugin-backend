3bf8d7b7e866fa284cc7caef79130238dc066e02
Vicențiu Ciorbaru <cvicentiu@gmail.com>
2023-03-15 12:38:13 +0200

    Extend extract_upload_facts to use multiple threads

--- src/feedback_plugin/management/commands/extract_upload_facts.py
+++ src/feedback_plugin/management/commands/extract_upload_facts.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor, wait
 from datetime import datetime, timedelta, timezone
 
 from django.core.management.base import BaseCommand
@@ -15,20 +16,30 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('start_time', type=Command.date_with_tz_from_str)
         parser.add_argument('end_time', type=Command.date_with_tz_from_str)
+        parser.add_argument('--workers', type=int, default=4,
+                            help='How many threads to use to compute upload '
+                                 'facts')
 
     def handle(self, *args, **options):
         start_time = options['start_time']
         end_time = options['end_time']
+        workers = options['workers']
 
-        while start_time + timedelta(seconds=60 * 60 * 24) <= end_time:
-            cur_end = start_time + timedelta(seconds=60 * 60 * 24)
+        with ThreadPoolExecutor(max_workers=workers) as executor:
+            jobs = []
+            while start_time + timedelta(seconds=60 * 60 * 24) <= end_time:
+                cur_end = start_time + timedelta(seconds=60 * 60 * 24)
 
-            etl.extract_upload_facts(start_time, cur_end,
+                job = executor.submit(etl.extract_upload_facts,
+                                      start_time, cur_end,
+                                      [extractors.AllUploadFactExtractor()],
+                                      end_inclusive=False)
+                jobs.append(job)
+                start_time = cur_end
+
+            wait(jobs)
+
+            etl.extract_upload_facts(start_time, end_time,
                                      [extractors.AllUploadFactExtractor()],
-                                     end_inclusive=False)
-            start_time = cur_end
-
-        etl.extract_upload_facts(start_time, end_time,
-                                 [extractors.AllUploadFactExtractor()],
-                                 end_inclusive=True)
+                                     end_inclusive=True)
 
