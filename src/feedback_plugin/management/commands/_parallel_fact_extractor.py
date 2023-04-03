ea1b31d3ac0c042e63a0943399f4900b18108bea
Vicențiu Ciorbaru <cvicentiu@gmail.com>
2023-04-03 17:23:11 +0300

    Eliminate code duplication in management commands
    extract_server_facts and extract_upload_facts have very simillar logic.
    Factor it out and use a base class.

---
+++ src/feedback_plugin/management/commands/_parallel_fact_extractor.py
@@ -0,0 +1,59 @@
+from concurrent.futures import ProcessPoolExecutor, wait
+from datetime import datetime, timedelta, timezone
+from typing import Callable
+
+from django.core.management.base import BaseCommand
+
+from ...data_processing import extractors
+
+
+class ProcessPoolFactExtractor(BaseCommand):
+    def __init__(
+            self,
+            extract_cb: Callable[[datetime, datetime,
+                                  list[extractors.DataExtractor], bool], None],
+            extractors: list[extractors.DataExtractor],
+            *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._extract_cb = extract_cb
+        self._extractors = extractors
+
+    @staticmethod
+    def date_with_tz_from_str(string: str) -> datetime:
+        date = datetime.strptime(string, '%Y-%m-%d')
+        date = date.replace(tzinfo=timezone.utc)
+        return date
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'start_time', type=ProcessPoolFactExtractor.date_with_tz_from_str)
+        parser.add_argument(
+            'end_time', type=ProcessPoolFactExtractor.date_with_tz_from_str)
+        parser.add_argument(
+            '--workers', type=int, default=4,
+            help='How many threads to use to compute facts')
+
+    def handle(self, *args, **options):
+        start_time = options['start_time']
+        end_time = options['end_time']
+        workers = options['workers']
+
+        with ProcessPoolExecutor(max_workers=workers) as executor:
+            jobs = []
+            while start_time + timedelta(seconds=60 * 60 * 24) <= end_time:
+                cur_end = start_time + timedelta(seconds=60 * 60 * 24)
+
+                job = executor.submit(self._extract_cb,
+                                      start_time, cur_end,
+                                      self._extractors,
+                                      [extractors.AllUploadFactExtractor()],
+                                      end_inclusive=False)
+                jobs.append(job)
+                start_time = cur_end
+
+            wait(jobs)
+
+            self._extract_cb(start_time, end_time,
+                             self._extractors,
+                             end_inclusive=True)
+
--- src/feedback_plugin/management/commands/extract_upload_facts.py
+++ src/feedback_plugin/management/commands/extract_upload_facts.py
@@ -1,45 +1,9 @@
-from concurrent.futures import ProcessPoolExecutor, wait
-from datetime import datetime, timedelta, timezone
-
-from django.core.management.base import BaseCommand
-
 from ...data_processing import etl, extractors
+from ._parallel_fact_extractor import ProcessPoolFactExtractor
 
 
-class Command(BaseCommand):
-    @staticmethod
-    def date_with_tz_from_str(string: str) -> datetime:
-        date = datetime.strptime(string, '%Y-%m-%d')
-        date = date.replace(tzinfo=timezone.utc)
-        return date
-
-    def add_arguments(self, parser):
-        parser.add_argument('start_time', type=Command.date_with_tz_from_str)
-        parser.add_argument('end_time', type=Command.date_with_tz_from_str)
-        parser.add_argument('--workers', type=int, default=4,
-                            help='How many threads to use to compute upload '
-                                 'facts')
-
-    def handle(self, *args, **options):
-        start_time = options['start_time']
-        end_time = options['end_time']
-        workers = options['workers']
-
-        with ProcessPoolExecutor(max_workers=workers) as executor:
-            jobs = []
-            while start_time + timedelta(seconds=60 * 60 * 24) <= end_time:
-                cur_end = start_time + timedelta(seconds=60 * 60 * 24)
-
-                job = executor.submit(etl.extract_upload_facts,
-                                      start_time, cur_end,
-                                      [extractors.AllUploadFactExtractor()],
-                                      end_inclusive=False)
-                jobs.append(job)
-                start_time = cur_end
-
-            wait(jobs)
-
-            etl.extract_upload_facts(start_time, end_time,
-                                     [extractors.AllUploadFactExtractor()],
-                                     end_inclusive=True)
-
+class Command(ProcessPoolFactExtractor):
+    def __init__(self, *args, **kwargs):
+        super().__init__(etl.extract_upload_facts,
+                         [extractors.AllUploadFactExtractor()],
+                         *args, **kwargs)
